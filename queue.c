5659382121ec7546906cb59edc6531e7b50dedaf
Atharv Agarwal <172584851+Agarwal-Atharv@users.noreply.github.com>
2025-04-04 23:05:22 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,8 +1,36 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
+    uint64_t serialized_state = serialize(state);
+    insert_at_tail(&(q->data), serialized_state);
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) {
+    uint64_t serialized_state = remove_from_head(&(q->data));
+    return deserialize(serialized_state);
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+int number_of_moves(struct game_state start) {
+    struct queue q;
+    q.data.head = NULL;
+
+    enqueue(&q, start);
+
+    while (q.data.head != NULL) {
+        struct game_state current_state = dequeue(&q);
+
+        if (is_goal_state(current_state)) {
+            return current_state.num_steps;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            struct game_state next_state = generate_next_state(current_state, i);
+            if (!is_state_visited(next_state)) {
+                enqueue(&q, next_state);
+            }
+        }
+    }
+
+    return -1;
+}
