eab5488fb7f2bc94f6224d510c8d0f55f0fff360
Atharv Agarwal <172584851+Agarwal-Atharv@users.noreply.github.com>
2025-04-04 23:13:15 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,36 +1,133 @@
 #include "queue.h"
 #include "tile_game.h"
+#include <stdlib.h>
+#include <stdbool.h>
 
-void enqueue(struct queue *q, struct game_state state) {
-    uint64_t serialized_state = serialize(state);
-    insert_at_tail(&(q->data), serialized_state);
+static bool is_goal_state(struct game_state state) {
+  int expected = 1;
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      if (i == 3 && j == 3) {
+        if (state.tiles[i][j] != 0)
+          return false;
+      } else {
+        if (state.tiles[i][j] != expected)
+          return false;
+        expected++;
+      }
+    }
+  }
+  return true;
 }
 
-struct game_state dequeue(struct queue *q) {
-    uint64_t serialized_state = remove_from_head(&(q->data));
-    return deserialize(serialized_state);
+static int bfs(struct game_state start) {
+  if (is_goal_state(start))
+    return start.num_steps;
+
+  struct queue q;
+  q.data.head = NULL;
+  struct linked_list visited;
+  visited.head = NULL;
+
+  uint64_t startSer = serialize(start);
+  insert_at_tail(&(q.data), (size_t)startSer);
+  insert_at_tail(&visited, (size_t)startSer);
+
+  while (q.data.head) {
+    uint64_t ser = (uint64_t)remove_from_head(&(q.data));
+    struct game_state cur = deserialize(ser);
+    if (is_goal_state(cur)) {
+      free_list(q.data);
+      free_list(visited);
+      return cur.num_steps;
+    }
+
+    struct game_state next;
+
+    next = cur;
+    move_up(&next);
+    if (next.num_steps != cur.num_steps) { 
+      uint64_t key = serialize(next);
+      bool seen = false;
+      for (struct list_node *n = visited.head; n; n = n->next) {
+        if (n->value == key) {
+          seen = true;
+          break;
+        }
+      }
+      if (!seen) {
+        insert_at_tail(&visited, (size_t)key);
+        insert_at_tail(&(q.data), (size_t)key);
+      }
+    }
+
+    next = cur;
+    move_down(&next);
+    if (next.num_steps != cur.num_steps) {
+      uint64_t key = serialize(next);
+      bool seen = false;
+      for (struct list_node *n = visited.head; n; n = n->next) {
+        if (n->value == key) {
+          seen = true;
+          break;
+        }
+      }
+      if (!seen) {
+        insert_at_tail(&visited, (size_t)key);
+        insert_at_tail(&(q.data), (size_t)key);
+      }
+    }
+
+    next = cur;
+    move_left(&next);
+    if (next.num_steps != cur.num_steps) {
+      uint64_t key = serialize(next);
+      bool seen = false;
+      for (struct list_node *n = visited.head; n; n = n->next) {
+        if (n->value == key) {
+          seen = true;
+          break;
+        }
+      }
+      if (!seen) {
+        insert_at_tail(&visited, (size_t)key);
+        insert_at_tail(&(q.data), (size_t)key);
+      }
+    }
+
+    next = cur;
+    move_right(&next);
+    if (next.num_steps != cur.num_steps) {
+      uint64_t key = serialize(next);
+      bool seen = false;
+      for (struct list_node *n = visited.head; n; n = n->next) {
+        if (n->value == key) {
+          seen = true;
+          break;
+        }
+      }
+      if (!seen) {
+        insert_at_tail(&visited, (size_t)key);
+        insert_at_tail(&(q.data), (size_t)key);
+      }
+    }
+  }
+
+  free_list(q.data);
+  free_list(visited);
+  return -1;
 }
 
 int number_of_moves(struct game_state start) {
-    struct queue q;
-    q.data.head = NULL;
+  return bfs(start);
+}
 
-    enqueue(&q, start);
+void enqueue(struct queue *q, struct game_state state) {
+  uint64_t ser = serialize(state);
+  insert_at_tail(&(q->data), (size_t)ser);
+}
 
-    while (q.data.head != NULL) {
-        struct game_state current_state = dequeue(&q);
-
-        if (is_goal_state(current_state)) {
-            return current_state.num_steps;
-        }
-
-        for (int i = 0; i < 4; i++) {
-            struct game_state next_state = generate_next_state(current_state, i);
-            if (!is_state_visited(next_state)) {
-                enqueue(&q, next_state);
-            }
-        }
-    }
-
-    return -1;
+struct game_state dequeue(struct queue *q) {
+  size_t ser = remove_from_head(&(q->data));
+  return deserialize((uint64_t)ser);
 }
