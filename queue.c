31f22516185694bcfcdc1528cbb74da9bd5eb5f1
Atharv Agarwal <172584851+Agarwal-Atharv@users.noreply.github.com>
2025-04-04 23:20:25 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,133 +1,117 @@
 #include "queue.h"
 #include "tile_game.h"
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
 
-static bool is_goal_state(struct game_state state) {
-  int expected = 1;
+void enqueue(struct queue *q, struct game_state state) {
+  uint64_t serialized = serialize(state);
+  insert_at_tail(&q->data, serialized);
+}
+
+struct game_state dequeue(struct queue *q) {
+  uint64_t serialized = remove_from_head(&q->data);
+  return deserialize(serialized);
+}
+
+static bool is_solved(struct game_state state) {
+  uint8_t solved[4][4] = {
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9, 10, 11, 12},
+    {13, 14, 15, 0}
+  };
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 4; j++) {
-      if (i == 3 && j == 3) {
-        if (state.tiles[i][j] != 0)
-          return false;
-      } else {
-        if (state.tiles[i][j] != expected)
-          return false;
-        expected++;
-      }
+      if (state.tiles[i][j] != solved[i][j])
+        return false;
     }
   }
   return true;
 }
 
-static int bfs(struct game_state start) {
-  if (is_goal_state(start))
+typedef struct {
+  uint64_t *data;
+  size_t size;
+  size_t capacity;
+} visited_set_t;
+
+static void init_visited(visited_set_t *set) {
+  set->capacity = 1024;
+  set->size = 0;
+  set->data = malloc(set->capacity * sizeof(uint64_t));
+  if (!set->data) exit(1);
+}
+
+static void free_visited(visited_set_t *set) {
+  free(set->data);
+}
+
+static bool visited_contains(visited_set_t *set, uint64_t state) {
+  for (size_t i = 0; i < set->size; i++) {
+    if (set->data[i] == state)
+      return true;
+  }
+  return false;
+}
+
+static void visited_add(visited_set_t *set, uint64_t state) {
+  if (set->size == set->capacity) {
+    set->capacity *= 2;
+    set->data = realloc(set->data, set->capacity * sizeof(uint64_t));
+    if (!set->data) exit(1);
+  }
+  set->data[set->size++] = state;
+}
+
+static struct game_state copy_state(const struct game_state *state) {
+  struct game_state copy;
+  memcpy(&copy, state, sizeof(struct game_state));
+  return copy;
+}
+
+typedef void (*move_func_t)(struct game_state *);
+
+static bool try_move(const struct game_state *orig, move_func_t move, struct game_state *result) {
+  *result = copy_state(orig);
+  move(result);
+  return serialize(*result) != serialize(*orig);
+}
+
+int number_of_moves(struct game_state start) {
+  if (is_solved(start))
     return start.num_steps;
 
   struct queue q;
   q.data.head = NULL;
-  struct linked_list visited;
-  visited.head = NULL;
+  enqueue(&q, start);
 
-  uint64_t startSer = serialize(start);
-  insert_at_tail(&(q.data), (size_t)startSer);
-  insert_at_tail(&visited, (size_t)startSer);
+  visited_set_t visited;
+  init_visited(&visited);
+  visited_add(&visited, serialize(start));
 
-  while (q.data.head) {
-    uint64_t ser = (uint64_t)remove_from_head(&(q.data));
-    struct game_state cur = deserialize(ser);
-    if (is_goal_state(cur)) {
+  while (q.data.head != NULL) {
+    struct game_state cur = dequeue(&q);
+    if (is_solved(cur)) {
+      free_visited(&visited);
       free_list(q.data);
-      free_list(visited);
       return cur.num_steps;
     }
 
-    struct game_state next;
+    move_func_t moves[4] = { move_up, move_down, move_left, move_right };
 
-    next = cur;
-    move_up(&next);
-    if (next.num_steps != cur.num_steps) { 
-      uint64_t key = serialize(next);
-      bool seen = false;
-      for (struct list_node *n = visited.head; n; n = n->next) {
-        if (n->value == key) {
-          seen = true;
-          break;
+    for (int i = 0; i < 4; i++) {
+      struct game_state next;
+      if (try_move(&cur, moves[i], &next)) {
+        uint64_t serialized_next = serialize(next);
+        if (!visited_contains(&visited, serialized_next)) {
+          visited_add(&visited, serialized_next);
+          enqueue(&q, next);
         }
-      }
-      if (!seen) {
-        insert_at_tail(&visited, (size_t)key);
-        insert_at_tail(&(q.data), (size_t)key);
-      }
-    }
-
-    next = cur;
-    move_down(&next);
-    if (next.num_steps != cur.num_steps) {
-      uint64_t key = serialize(next);
-      bool seen = false;
-      for (struct list_node *n = visited.head; n; n = n->next) {
-        if (n->value == key) {
-          seen = true;
-          break;
-        }
-      }
-      if (!seen) {
-        insert_at_tail(&visited, (size_t)key);
-        insert_at_tail(&(q.data), (size_t)key);
-      }
-    }
-
-    next = cur;
-    move_left(&next);
-    if (next.num_steps != cur.num_steps) {
-      uint64_t key = serialize(next);
-      bool seen = false;
-      for (struct list_node *n = visited.head; n; n = n->next) {
-        if (n->value == key) {
-          seen = true;
-          break;
-        }
-      }
-      if (!seen) {
-        insert_at_tail(&visited, (size_t)key);
-        insert_at_tail(&(q.data), (size_t)key);
-      }
-    }
-
-    next = cur;
-    move_right(&next);
-    if (next.num_steps != cur.num_steps) {
-      uint64_t key = serialize(next);
-      bool seen = false;
-      for (struct list_node *n = visited.head; n; n = n->next) {
-        if (n->value == key) {
-          seen = true;
-          break;
-        }
-      }
-      if (!seen) {
-        insert_at_tail(&visited, (size_t)key);
-        insert_at_tail(&(q.data), (size_t)key);
       }
     }
   }
 
-  free_list(q.data);
-  free_list(visited);
+  free_visited(&visited);
   return -1;
-}
-
-int number_of_moves(struct game_state start) {
-  return bfs(start);
-}
-
-void enqueue(struct queue *q, struct game_state state) {
-  uint64_t ser = serialize(state);
-  insert_at_tail(&(q->data), (size_t)ser);
-}
-
-struct game_state dequeue(struct queue *q) {
-  size_t ser = remove_from_head(&(q->data));
-  return deserialize((uint64_t)ser);
 }
